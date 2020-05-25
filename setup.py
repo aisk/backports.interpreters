020c92a876df405e3951dd27fceb6dbabbf9e26b
An Long <aisk1988@gmail.com>
2020-05-26 00:08:18 +0800

    :wrench: add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="backports.interpreters",
+    description='Backport of Python interpreters module described in PEP554',
+    author="An Long",
+    url="https://github.com/aisk/backports.interpreters",
+    version="0.0.1",
+    py_modules=["backports.interpreters"],
+    namespace_packages=["backports"],
+    python_requires=">=3.8",
+)
