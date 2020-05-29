8a1cc59f982e7d41f50712a2531dc5e54448d10b
An Long <aisk1988@gmail.com>
2020-05-30 00:15:13 +0800

    :bookmark: bump version 0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Backport of Python interpreters module described in PEP554',
     author="An Long",
     url="https://github.com/aisk/backports.interpreters",
-    version="0.0.1",
+    version="0.1.0",
     py_modules=["backports.interpreters"],
     namespace_packages=["backports"],
     python_requires=">=3.8",
