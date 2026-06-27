f18d2db60aac8ba4137c647f042016b318ede248
An Long <aisk1988@gmail.com>
2026-06-27 23:41:23 +0900

    📄 relicense under PSF License Version 2

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="backports.interpreters",
     description="Backport of the concurrent.interpreters module described in PEP 734",
-    author="AN Long",
+    author="An Long",
     url="https://github.com/aisk/backports.interpreters",
     version="0.3.0",
+    license="PSF-2.0",
     packages=find_namespace_packages(include=["backports.*"]),
     python_requires=">=3.8",
     classifiers=[
+        "License :: OSI Approved :: Python Software Foundation License",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
